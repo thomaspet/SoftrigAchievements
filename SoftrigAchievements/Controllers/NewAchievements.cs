28e9c67efc7c99b6c9bf0504c709a654a356c10a
Eirik Eide Saur <eiriks@unimicro.no>
2022-10-07 08:19:54 +0200

    update recieved, use claimsprincipal

--- SoftrigAchievements/Controllers/NewAchievements.cs
+++ SoftrigAchievements/Controllers/NewAchievements.cs
@@ -7,13 +7,15 @@ namespace SoftrigAchievements.Controllers;
 
 public class NewAchievements
 {
-    public static List<Achievement> GetNewAchievements(HttpContext httpContext, Context database)
+    public static List<Achievement> GetNewAchievements(ClaimsPrincipal user, Context database)
     {
-        var user = httpContext.User;
         var globalIdentity = user.FindFirstValue(ClaimTypes.NameIdentifier);
         var achievementForUsers = database.AchievementForUsers.Where(x => x.Achieved && !x.Recieved && x.User == globalIdentity).Include(x => x.Achievement).ToList();
         if (achievementForUsers == null || !achievementForUsers.Any()) return new List<Achievement>();
         var achievements = achievementForUsers.Where(x => x.Achievement != null)?.Select(x => x.Achievement).ToList();
+        achievementForUsers.ForEach(x => x.Recieved = true);
+        database.UpdateRange(achievementForUsers);
+        database.SaveChanges();
         return achievements ?? new List<Achievement>();
     }
 }
